628f0c9e9ebfa6d1a0bd545baaf9733bd5742c24
James Cox <jamesncox@yahoo.com>
2019-06-20 15:30:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < Sinatra::Base
   post '/sessions' do
     # @user = User.find_by(email: params["email"], password: params["password"])
     # session[:user_id] = @user.id
-    binding.pry
+
     @user = User.find_by(email: params[:email], password: params[:password])
     if @user
       session[:user_id] = @user.id
